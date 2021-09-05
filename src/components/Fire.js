6fd9e03ec63530b2e5c52515708855b8118a41a9
waqas saleem <waqas.mba86@gmail.com>
2021-09-05 12:57:05 +0500

    my online available documents

---
+++ src/components/Fire.js
@@ -0,0 +1,35 @@
+import firebase from 'firebase/app'
+import 'firebase/database'
+import 'firebase/auth'
+import 'firebase/storage'
+
+
+
+var firebaseConfig = {
+  apiKey: "AIzaSyC-KgW3rwDB-Ze_2Ilog-_BbHsA5V8R90M",
+  authDomain: "my-docs-67943.firebaseapp.com",
+  projectId: "my-docs-67943",
+  storageBucket: "my-docs-67943.appspot.com",
+  messagingSenderId: "153064913489",
+  appId: "1:153064913489:web:d1e780db3f23c6bc0b5670",
+  measurementId: "G-52KX61XWHB"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+//   firebase.analytics();
+
+
+export default firebase;
+
+
+
+
+
+
+    // apiKey: "AIzaSyCRKceXGhdhpKFQiAJ2Qgkb1wXgXbI36RQ",
+    // authDomain: "ngrpractice.firebaseapp.com",
+    // projectId: "ngrpractice",
+    // storageBucket: "ngrpractice.appspot.com",
+    // messagingSenderId: "384804225359",
+    // appId: "1:384804225359:web:d1c84f17b1de731c99853a",
+    // measurementId: "G-NKKRC8FGHL"
